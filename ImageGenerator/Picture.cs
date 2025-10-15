00745d08c762bebeed4fcc938b38779cad19520b
Aliyahs Development <aliyahsdevelopment@gmail.com>
2025-10-15 12:51:38 +0200

    Add Picture model class
    This class includes File, Name and Favorite.

---
+++ ImageGenerator/Picture.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageGenerator
+{
+    internal class Picture
+    {
+        public string File { get; set; }
+        public string Name { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
